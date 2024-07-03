27ef98f48b9a5c3b195f7ff43b3efaac916cf94f
FoxTeam289 <programmer.danya.fox@gmail.com>
2024-07-03 15:42:03 +0300

    Delete save_profiles.php

--- save_profiles.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $profiles = file_get_contents('php://input');
-    file_put_contents('profiles.json', $profiles);
-    echo 'Профили сохранены';
-}
-?>
